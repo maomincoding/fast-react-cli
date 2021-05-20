592b4db377f45cdd392bc96f8451f58380be3cf9
maomincoding <17864296568@163.com>
2021-05-20 14:10:18 +0800

    v1.2.6

--- bin/config/index.js
+++ bin/config/index.js
@@ -3,22 +3,22 @@
   @Author: tree
  */
 module.exports = {
-  npmUrl: 'https://registry.npmjs.org/fast-react-cli',
-  promptTypeList: [
-    {
-      type: 'list',
-      message: 'Please select the template type to pull:',
-      name: 'type',
-      choices: [
-        {
-          name: 'fast-react-app@1.0.1',
-          value: {
-            url: 'https://github.com/maomincoding/fast-react-app.git',
-            gitName: 'fast-react-app',
-            val: 'fast-react-app',
-          },
-        },
-      ],
-    },
-  ],
+	npmUrl: 'https://registry.npmjs.org/fast-react-cli',
+	promptTypeList: [
+		{
+			type: 'list',
+			message: 'Please select the template type to pull:',
+			name: 'type',
+			choices: [
+				{
+					name: 'fast-react-app@1.0.2',
+					value: {
+						url: 'https://github.com/maomincoding/fast-react-app.git',
+						gitName: 'fast-react-app',
+						val: 'fast-react-app',
+					},
+				},
+			],
+		},
+	],
 };
