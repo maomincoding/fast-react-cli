93ddcd9a0ad68a00745422954419cbbe34156b7e
maomincoding <17864296568@163.com>
2021-05-20 14:21:25 +0800

    v1.2.7

--- bin/config/index.js
+++ bin/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
 			name: 'type',
 			choices: [
 				{
-					name: 'fast-react-app@1.0.2',
+					name: 'fast-react-app@1.1.0',
 					value: {
 						url: 'https://github.com/maomincoding/fast-react-app.git',
 						gitName: 'fast-react-app',
